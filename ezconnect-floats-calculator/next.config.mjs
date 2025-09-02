e585fcb2f47300799caf0f98e8184646db2f29d1
vacebuque <gino.reyes@eddypump.com>
2025-09-03 07:49:58 +0800

    chore: replace next.config.ts with next.config.mjs

---
+++ ezconnect-floats-calculator/next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // add your options here (images, eslint, experimental, etc.)
+};
+
+export default nextConfig;
